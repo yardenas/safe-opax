30e6e1c8cb475cb5a6d9a877acff00e2d21df843
Yarden <yarden.as@inf.ethz.ch>
2024-04-12 16:14:35 +0200

    Add exploration

--- safe_opax/la_mbda/la_mbda.py
+++ safe_opax/la_mbda/la_mbda.py
@@ -3,6 +3,7 @@ from typing import NamedTuple
 import equinox as eqx
 import jax
 import jax.numpy as jnp
+import distrax as dtx
 import numpy as np
 from gymnasium.spaces import Box
 from omegaconf import DictConfig
@@ -31,6 +32,10 @@ def policy(actor, model, prev_state, observation, key):
             prev_state.rssm_state, observation, prev_state.prev_action, model_key
         )
         action = actor.act(current_rssm_state.flatten(), policy_key)
+        # FIXME (yarden): don't
+        action = dtx.Normal(action, 0.3 * jnp.ones_like(action)).sample(
+            seed=policy_key
+        )
         return action, AgentState(current_rssm_state, action)
 
     observation = preprocess(observation)
