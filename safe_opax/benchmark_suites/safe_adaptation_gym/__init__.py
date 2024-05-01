b861f75d99288c0dc83795d77ddd3b229cd6c414
Yarden <yarden.as@inf.ethz.ch>
2024-05-01 11:19:18 +0200

    Fix env compatibility bug

--- safe_opax/benchmark_suites/safe_adaptation_gym/__init__.py
+++ safe_opax/benchmark_suites/safe_adaptation_gym/__init__.py
@@ -1,6 +1,6 @@
+from gymnasium.wrappers.compatibility import EnvCompatibility
 from omegaconf import DictConfig
 import numpy as np
-from gymnasium.wrappers.compatibility import EnvCompatibility
 from safe_opax.benchmark_suites.utils import get_domain_and_task
 
 from safe_opax.rl.types import EnvironmentFactory
@@ -20,6 +20,16 @@ def sample_task(seed):
     return task
 
 
+class SafeAdaptationEnvCompatibility(EnvCompatibility):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def reset(self, seed=None, options=None):
+        if seed is not None:
+            self.env.seed(seed)
+        return self.env.reset(options=options), {}
+
+
 def make(cfg: DictConfig) -> EnvironmentFactory:
     def make_env():
         import safe_adaptation_gym
@@ -36,7 +46,7 @@ def make(cfg: DictConfig) -> EnvironmentFactory:
             rgb_observation=task_cfg.image_observation.enabled,
             render_lidar_and_collision=not task_cfg.image_observation.enabled,
         )
-        env = EnvCompatibility(env)
+        env = SafeAdaptationEnvCompatibility(env)
         if (
             task_cfg.image_observation.enabled
             and task_cfg.image_observation.image_format == "channels_first"
