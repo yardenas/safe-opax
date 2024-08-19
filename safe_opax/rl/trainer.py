3bb4708124bbf19c3c730612a8f1753e584b69c6
Yarden <yarden.as@inf.ethz.ch>
2024-08-19 09:59:30 +0200

    No need to really reset tasks for now

--- safe_opax/rl/trainer.py
+++ safe_opax/rl/trainer.py
@@ -233,7 +233,7 @@ class UnsupervisedTrainer(Trainer):
                 get_task(self.test_task_name)
                 for _ in range(self.config.training.parallel_envs)
             ]
-            self.env.reset(options={"task": self.test_tasks})
+            # self.env.reset(options={"task": self.test_tasks})
         return self
 
     def _run_training_epoch(
@@ -250,7 +250,7 @@ class UnsupervisedTrainer(Trainer):
                 for _ in range(self.config.training.parallel_envs)
             ]
             assert self.env is not None
-            self.env.reset(options={"task": self.test_tasks})
+            # self.env.reset(options={"task": self.test_tasks})
             assert self.agent is not None
         return outs
 
